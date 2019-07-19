8e915c03e9ec4720c5c347e9240b57c93214c739
PRAVEEN CHILIVERI <praveenchiliveri6@gmail.com>
2019-07-19 15:20:18 +0530

    creating excel file

--- BugWriter.java
+++ BugWriter.java
@@ -1,40 +1,90 @@
 package com.philips.casestudy;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.*;
 
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class BugWriter {
-	public static void writeData(ArrayList<FindBugsData> buglist, String Classname) throws Exception
-	{
+	public static void writeFindBugsData(ArrayList<FindBugsData> buglist, String Classname, String name)
+			throws Exception {
 		try {
-		PrintWriter pw  = new PrintWriter("C:\\Users\\320066613\\CodeAnalysis\\Reports\\"+Classname+".txt", "UTF-8");
-		for (FindBugsData f : buglist)
-		{
-			pw.println("Error Category: "+f.getCategory());
-			pw.println("Error type: "+f.getType());
-			pw.println("in Class: "+f.getClassName()+"\n");
+			XSSFWorkbook workbook = new XSSFWorkbook();
+			XSSFSheet sheet = workbook.createSheet("Findbugs");
+			Map<Integer, Object[]> data = new TreeMap<>();
+			data.put(1, new Object[] { "Class Name", "Error Category", "Error Type" });
+			int i = 2;
+			for (FindBugsData f : buglist) {
+				data.put(i++, new Object[] { f.getClassName(), f.getCategory(), f.getType() });
+			}
+
+			Set<Integer> keyset = data.keySet();
+			int rownum = 0;
+			for (Integer key : keyset) {
+				Row row = sheet.createRow(rownum++);
+				Object[] objArr = data.get(key);
+				int cellnum = 0;
+				for (Object obj : objArr) {
+					Cell cell = row.createCell(cellnum++);
+					sheet.setColumnWidth(cellnum - 1, 15000);
+					cell.setCellValue((String) obj);
+				}
+			}
+
+			FileOutputStream out = new FileOutputStream(
+					new File("C:\\Users\\320065410\\OneDrive - Philips\\Desktop\\Reports\\finalreport\\" + Classname
+							+ name + ".xlsx"));
+			workbook.write(out);
+			out.close();
+			workbook.close();
+
 		}
-		pw.close();
+
+		catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch(Exception e)
-		{e.printStackTrace();}
-		}
-	
-	public static void writeDataPMD(ArrayList<PMDData> buglist, String Classname) throws Exception
-	{
+	}
+
+	public static void writePmdData(ArrayList<PmdData> buglist, String Classname, String name) throws Exception {
 		try {
-		PrintWriter pw  = new PrintWriter("C:\\Users\\320066613\\CodeAnalysis\\Reports\\"+Classname+".txt","UTF-8");
-		for (PMDData f : buglist)
-		{
-			pw.println("Error Category: "+f.getIssue());
-			pw.println("Error details: "+f.getData());
-			pw.println("in Class: "+f.getClassName()+"\n");
+
+			XSSFWorkbook workbook = new XSSFWorkbook();
+			XSSFSheet sheet = workbook.createSheet("PMD");
+			Map<Integer, Object[]> data = new TreeMap<>();
+			data.put(1, new Object[] { "Class Name","Line Number", "RuleSet", "Error" });
+			int i = 2;
+			for (PmdData f : buglist) {
+				data.put(i++, new Object[] { f.getClassName(),f.getLine(), f.getruleSet(), f.getContent() });
+			}
+
+			Set<Integer> keyset = data.keySet();
+			int rownum = 0;
+			for (Integer key : keyset) {
+				Row row = sheet.createRow(rownum++);
+				Object[] objArr = data.get(key);
+				int cellnum = 0;
+				for (Object obj : objArr) {
+					Cell cell = row.createCell(cellnum++);
+					sheet.setColumnWidth(cellnum - 1, 10000);
+					cell.setCellValue((String) obj);
+				}
+			}
+
+			FileOutputStream out = new FileOutputStream(
+					new File("C:\\Users\\320065410\\OneDrive - Philips\\Desktop\\Reports\\finalreport\\" + Classname
+							+ name + ".xlsx"));
+			workbook.write(out);
+			out.close();
+			workbook.close();
 		}
-		pw.close();
+
+		catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch(Exception e)
-		{e.printStackTrace();}
-		}
-	
+	}
+
 }
