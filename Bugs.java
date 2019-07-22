fb3562f8ade02a61bdfc498ecfa932ecd8dcd8ab
PRAVEEN CHILIVERI <praveenchiliveri6@gmail.com>
2019-07-22 15:29:21 +0530

    removed

--- Bugs.java
+++
@@ -1,7 +0,0 @@
-package com.philips.casestudy;
-
-
-public interface Bugs<T> {
-	public void getBugs(String projectName, String analyserName);
-	public  void writeData(String Classname, String AnalysisType);
-}
